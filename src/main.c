fa70b254b016f7011041e0906442614263d73ccc
Nevyn Bengtsson <nevyn@alloverse.com>
2021-06-11 17:37:28 +0200

    run and keep running marketplace

--- src/main.c
+++ src/main.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 #include <allonet/server.h>
+
+static int marketplace_pid = -1;
+
+static void
+child_handler(int sig)
+{
+    pid_t pid;
+    int status;
+
+    while((pid = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+        if(pid == marketplace_pid)
+        {
+            fprintf(stderr, "Reaping marketplace at %d\n", pid);
+            marketplace_pid = -1; // trigger relaunch
+        }
+    }
+}
+
+static void ensure_marketplace_running()
+{
+    if(marketplace_pid == -1)
+    {
+        marketplace_pid = fork();
+        if(marketplace_pid == 0)
+        {
+            fprintf(stderr, "Launching marketplace...\n");
+            exit(system("cd marketplace; ./allo/assist run alloplace://localhost"));
+        }
+    }
+}
 
 int main(int argc, const char **argv)
 {
@@ -9,5 +45,35 @@ int main(int argc, const char **argv)
         return 1;
     }
     const char *placename = argv[1];
-    return alloserv_run_standalone(0, 21337, placename);
+
+    printf("Launching alloplace2 as '%s'\n", placename);
+    alloserver *serv = alloserv_start_standalone(0, 21337, placename);
+  
+    if (serv == NULL)
+    {
+        return false;
+    }
+
+    int allosocket = allo_socket_for_select(serv);
+
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = child_handler;
+    sigaction(SIGCHLD, &sa, NULL);
+
+    int house_ok = system("cd marketplace/apps/allo-house; ./allo/assist run alloplace://localhost &");
+    
+    printf("alloplace2 is now entering runloop.\n");
+    while (1) {
+        if (alloserv_poll_standalone(allosocket) == false)
+        {
+            alloserv_stop_standalone();
+            return false;
+        }
+        ensure_marketplace_running();
+    }
+
+    alloserv_stop_standalone();
+    return 0;
 }
